53e5daed1b32465f40a29a69e10ea8fade8d9cf2
yeonii01 <adencho1204@naver.com>
2022-12-14 23:16:41 +0900

    컴퓨터 그래픽스 완성 코드

--- texturefragment.glsl
+++ texturefragment.glsl
@@ -3,23 +3,26 @@
 in vec3 FragPos;
 in vec3 Normal;
 uniform vec3 outColor;
+in vec2 Tex;
 
 uniform vec3 cameraPos;
 uniform vec3 lightPos;
 uniform vec3 lightColor;
-
+uniform sampler2D outTex;
+uniform float alpha;
+uniform int select;
 out vec4 fColor;
 
 void main ()
 {
-  vec3 ambientLight = vec3(0.5f, 0.5f, 0.5f);
+  vec3 ambientLight = vec3(0.3f, 0.3f, 0.3f);
   vec3 ambient = ambientLight * lightColor;
   vec3 NV = normalize(Normal);
   vec3 lightDir = normalize(lightPos - FragPos);
   float diffuseLight = max(dot(NV, lightDir), 0.0);
   vec3 diffuse = diffuseLight * lightColor;
 
-  int shininess = 256;
+  int shininess = 128;
   vec3 viewDir = normalize(cameraPos - FragPos);
   vec3 reflectDir = reflect(lightDir, NV);
   float specularLight = max (dot(viewDir, reflectDir), 0.0);
@@ -28,5 +31,8 @@ void main ()
 
   vec3 result = (diffuse+specular+ambient) * outColor;
 
-  fColor = vec4((result), 1.0);
+	fColor = vec4((result), alpha);
+  if(select == 1)
+	fColor = texture(outTex, Tex) * fColor;
+ 
 }
